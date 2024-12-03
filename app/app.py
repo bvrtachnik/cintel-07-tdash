60ea580b1c6fbd517063546a1b94d355bfbfc5d5
bvrtachnik <b.vrtachnik@gmail.com>
2024-12-03 17:44:47 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,13 +5,22 @@ from shiny import reactive
 from shiny.express import input, render, ui
 import palmerpenguins 
 
+# Import theme from shinyswatch to use Bootswatch themes
+from shinyswatch import theme
+
+# Load Palmer Penguins dataset
 df = palmerpenguins.load_penguins()
 
-ui.page_opts(title="Penguins dashboard", fillable=True)
+# Set the page options
+ui.page_opts(title="Penguins dashboard", theme=theme.cyborg, fillable=True)
 
 
 with ui.sidebar(title="Filter controls"):
+
+    # Slider input to filter by mass
     ui.input_slider("mass", "Mass", 2000, 6000, 6000)
+
+    # Checkbox Group to filter by penguin species
     ui.input_checkbox_group(
         "species",
         "Species",
@@ -19,7 +28,9 @@ with ui.sidebar(title="Filter controls"):
         selected=["Adelie", "Gentoo", "Chinstrap"],
     )
     ui.hr()
-    ui.h6("Links")
+    ui.h6("Links") # Header for Links section
+
+    # Clickable links to resources
     ui.a(
         "GitHub Source",
         href="https://github.com/denisecase/cintel-07-tdash",
@@ -47,15 +58,17 @@ with ui.sidebar(title="Filter controls"):
         target="_blank",
     )
 
-
+# Main content area (outputs)
 with ui.layout_column_wrap(fill=False):
+
+    # Value box showing number of penguins in the filtered dataset
     with ui.value_box(showcase=icon_svg("earlybirds")):
         "Number of penguins"
 
         @render.text
         def count():
             return filtered_df().shape[0]
-
+    # Value box showing avg bill length of penguins in the filtered dataset
     with ui.value_box(showcase=icon_svg("ruler-horizontal")):
         "Average bill length"
 
@@ -63,6 +76,7 @@ with ui.layout_column_wrap(fill=False):
         def bill_length():
             return f"{filtered_df()['bill_length_mm'].mean():.1f} mm"
 
+     # Value box showing avg bill depth of penguins in the filtered dataset
     with ui.value_box(showcase=icon_svg("ruler-vertical")):
         "Average bill depth"
 
@@ -73,6 +87,8 @@ with ui.layout_column_wrap(fill=False):
 
 with ui.layout_columns():
     with ui.card(full_screen=True):
+
+        # Seaborn scatterplot showing Bill length vs. depth
         ui.card_header("Bill length and depth")
 
         @render.plot
@@ -81,11 +97,13 @@ with ui.layout_columns():
                 data=filtered_df(),
                 x="bill_length_mm",
                 y="bill_depth_mm",
-                hue="species",
+                hue="species",    # Color datapoints by species
             )
 
     with ui.card(full_screen=True):
-        ui.card_header("Penguin da")
+
+        # Data Grid showing summary stats of filtered dataset
+        ui.card_header("Penguin Data")
 
         @render.data_frame
         def summary_statistics():
@@ -101,7 +119,7 @@ with ui.layout_columns():
 
 #ui.include_css(app_dir / "styles.css")
 
-
+# Reactive function to filter dataset based on user inputs
 @reactive.calc
 def filtered_df():
     filt_df = df[df["species"].isin(input.species())]
